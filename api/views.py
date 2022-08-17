6d805141af8f49a58be598909672d52d1154b0e1
mahdi norouzi <mdn1376@gmail.com>
2022-08-17 10:01:45 +0430

    permissions.py added in api app and used from it.

--- api/views.py
+++ api/views.py
@@ -4,25 +4,28 @@ from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
+from .permissions import IsSuperUserOrStaffOrReadOnly, IsAuthorOrReadOnly, IsStaffOrReadOnly
 
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly)
 
 
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly)
 
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffOrReadOnly,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffOrReadOnly,)
