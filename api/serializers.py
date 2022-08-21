1145473baf87a39fe3af1116241247fa40a4ded0
mahdi norouzi <mdn1376@gmail.com>
2022-08-21 19:51:17 +0430

    used nested objects in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,15 @@ from blog.models import Article
 from django.contrib.auth import get_user_model
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'first_name', 'last_name']
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+
     class Meta:
         model = Article
         fields = '__all__'
@@ -21,4 +29,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = '__all__'
-
